1803b9a2a5234a6d875d9554dea1b3e67d142820
1noseA <1noseA2L@gmail.com>
2025-01-11 23:03:04 +0900

    リファクタリング

--- lib/main.dart
+++ lib/main.dart
@@ -30,20 +30,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Video> videos = List.generate(5, (index) => Video(
+    title: 'おすすめキャロットケーキ5選',
+    viewsAndDate: 'X.X万回再生・X日前',
+    imageUrl: 'https://sozaino.site/wp-content/uploads/2021/09/lunch50.png',
+  ));
 
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: Text(
           'YouTube',
           style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -71,19 +76,19 @@ class _MyHomePageState extends State<MyHomePage> {
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
-                      )
+                      ),
                     ),
                     Text(
                       '@carrotcakediary',
                       style: TextStyle(
                         fontSize: 14,
-                      )
+                      ),
                     ),
                     Text(
                       'チャンネル登録者数 X.XX万人',
                       style: TextStyle(
                         fontSize: 14,
-                      )
+                      ),
                     ),
                   ],
                 ),
@@ -102,169 +107,82 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 140,
-                  height: 100,
-                  child: Image.network('https://sozaino.site/wp-content/uploads/2021/09/lunch50.png'),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'おすすめキャロットケーキ5選',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        )
-                      ),
-                      Text(
-                        'X.X万回再生・X日前',
-                        style: TextStyle(
-                          fontSize: 14,
-                        )
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 140,
-                  height: 100,
-                  child: Image.network('https://sozaino.site/wp-content/uploads/2021/09/lunch50.png'),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'おすすめキャロットケーキ5選',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        )
-                      ),
-                      Text(
-                        'X.X万回再生・X日前',
-                        style: TextStyle(
-                          fontSize: 14,
-                        )
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 140,
-                  height: 100,
-                  child: Image.network('https://sozaino.site/wp-content/uploads/2021/09/lunch50.png'),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'おすすめキャロットケーキ5選',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        )
-                      ),
-                      Text(
-                        'X.X万回再生・X日前',
-                        style: TextStyle(
-                          fontSize: 14,
-                        )
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 140,
-                  height: 100,
-                  child: Image.network('https://sozaino.site/wp-content/uploads/2021/09/lunch50.png'),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'おすすめキャロットケーキ5選',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        )
-                      ),
-                      Text(
-                        'X.X万回再生・X日前',
-                        style: TextStyle(
-                          fontSize: 14,
-                        )
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 140,
-                  height: 100,
-                  child: Image.network('https://sozaino.site/wp-content/uploads/2021/09/lunch50.png'),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'おすすめキャロットケーキ5選',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        )
-                      ),
-                      Text(
-                        'X.X万回再生・X日前',
-                        style: TextStyle(
-                          fontSize: 14,
-                        )
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: videos.length,
+              itemBuilder: (context, index) {
+                return VideoCard(
+                  title: videos[index].title,
+                  viewsAndDate: videos[index].viewsAndDate,
+                  imageUrl: videos[index].imageUrl,
+                );
+              },
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Video {
+  final String title;
+  final String viewsAndDate;
+  final String imageUrl;
+
+  Video({
+    required this.title,
+    required this.viewsAndDate,
+    required this.imageUrl,
+  });
+}
+
+class VideoCard extends StatelessWidget {
+  final String title;
+  final String viewsAndDate;
+  final String imageUrl;
+
+  const VideoCard({
+    super.key, // ここを修正
+    required this.title,
+    required this.viewsAndDate,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 140,
+          height: 100,
+          child: Image.network(imageUrl),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                viewsAndDate,
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
